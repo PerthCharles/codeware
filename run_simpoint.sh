2e8a7df59b94727f499c0245544f7ccf3112cbb1
Perth Charles <zhongbincharles@gmail.com>
2014-11-03 22:57:57 +0800

    re-arrange files

--- run_simpoint.sh
+++
@@ -1,4 +0,0 @@
-#usage ./run_simpoint [file to analyze] 
-#example ./run_simpoint 401
-cd /home/zhongbin/topmc_simpoint_analyze/simpoint.3.2/bin;
-./sample.sh ${1}; 
--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    unsigned char a = 0xa5;
-    printf("%d\n",~a);
-
-    return 0;
-}
